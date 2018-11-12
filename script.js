7d3e0ccbba343b9ea4f56625a9f1173bff4d1c9f
Patrick Hogan <phogan110@live.com>
2018-11-12 16:10:01 -0700

    created describe method; added describe to `Square`

--- script.js
+++ script.js
@@ -12,27 +12,39 @@ function getRndInteger(min, max) {
 
 class Shape {
     constructor() {
-    }
+        
+    };
+};
+
+Shape.prototype.describe = function() {
+    
 };
 
 class Square extends Shape {
     constructor () {
-        super()
-        let squareLength = document.getElementsByClassName('square-side')[0].value
+        super();
+        let squareLength = document.getElementsByClassName('square-side')[0].value;
         
         this.element = document.createElement('div');
-        this.element.className = 'square'
+        this.element.className = 'square';
         this.element.setAttribute('style', "width: " + squareLength + "px; height: " + squareLength + "px; bottom: " + getRndInteger(124, 564) + "px; left: " + getRndInteger(8, 1045) + "px");
-    }
-}
+    };
+    describe() {
+        shapeName.textContent = this.element.className;
+        shapeWidth.textContent = parseInt(this.element.style.width);
+        shapeHeight.textContent = parseInt(this.element.style.height);
+        shapeArea.textContent = parseInt(this.element.style.width) * parseInt(this.element.style.height);
+        shapePerim.textContent = parseInt(this.element.style.width) * 2 + parseInt(this.element.style.height) * 2;
+    };
+};
 
 document.getElementsByClassName('square-btn')[0].addEventListener('click', function() {
     let addSquare = function() {
         const squareDiv = new Square();
-        shapeCanvas.appendChild(squareDiv.element)
+        shapeCanvas.appendChild(squareDiv.element);
         squareDiv.element.addEventListener('click', function() {
-            
-        })
+            squareDiv.describe();
+        });
     };
     if (document.getElementsByClassName('square-side')[0].value !== '') {
         addSquare();
